2a08a1d4fd45f4c586b42112241019049d55d4a9
happine2s <happine2s@sookmyung.ac.kr>
2025-11-25 03:03:12 +0900

    Feat: 각 데이터에 대한 Dto 구현

---
+++ src/main/java/smwu/network/dto/TemperatureRequest.java
@@ -0,0 +1,14 @@
+package smwu.network.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TemperatureRequest {
+    private String deviceId;
+    private double temperature;
+    private String timestamp; // YYYY-MM-DD HH:MM:SS
+}
