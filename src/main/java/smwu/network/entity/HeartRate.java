c63bbab4334a0913ad4c77bd7c7446bf2e6f8fa7
happine2s <happine2s@sookmyung.ac.kr>
2025-11-25 03:03:27 +0900

    Feat: 각 데이터에 대한 엔티티 구현

---
+++ src/main/java/smwu/network/entity/HeartRate.java
@@ -0,0 +1,26 @@
+package smwu.network.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HeartRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id")
+    private Device device;
+
+    private int heartRate;
+
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/smwu/network/entity/Temperature.java
@@ -0,0 +1,26 @@
+package smwu.network.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Temperature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id")
+    private Device device;
+
+    private double temperature;
+
+    private LocalDateTime timestamp;
+}
